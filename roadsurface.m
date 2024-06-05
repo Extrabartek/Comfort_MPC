f7a32f42ca422eb429411c30f29b4c1e5a91d7a0
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-06-05 13:27:08 +0200

    Corrected isolated bump

--- roadsurface.m
+++ roadsurface.m
@@ -4,7 +4,7 @@
 % -> Bump sequence 
 % -> Table top
 % -> Sharp 
-
+%
 %
 % Dimension site: https://www.dimensions.com/element/speed-bump-standard
 %%%
@@ -18,8 +18,8 @@ tValues = 0:1/f:10; % [s] Array of to be evaluated timesteps
 % Tunable parametrs (dependent on bump surface)
 A = 0.3;            % m
 V = 36 / 3.6;       % km/h
-l = 10;             % m
-L = 10;             % m
+l = 30;             % m
+L = 60;             % m
 
 %Generate profile
 profile = isolatedBump(tValues, A, V, l, L);
@@ -33,17 +33,34 @@ xlabel('time [s]')
 
 
 function heightProfile = isolatedBump(tValues, A, V, l, L)
+    % Generate an isolated bump, based on the specified input criteria.
+    %
+    % Inputs:
+    % -------
+    % tValues = 1D array of the time values at which the road surface should 
+    % be created.
+    % A       = Amplitude of the bump in meter
+    % V       = Velocity of the vehicle in meter per second
+    % l       = Start position of the bump in meter
+    % L       = Length of the bump in meter
+    % Outputs:
+    % -------
+    % heightProfile = 1D array containing the height profile with values 
+    %                   provided in meter [0,0,0,0,0.1,0.3,...]
+
+    heightProfile = zeros(1,length(tValues));
     
-    heightProfile = [];
-    
-    for t = tValues
+    for idx = 1:length(tValues)
         
-        if (l / V) < t && t < (l + L) / V
-            outHeight = (A / 2) * (1 - cos((2*pi*V*t)/L));
+        t = tValues(idx);
+        
+        if ((l / V) <= t) && (t <= (l + L) / V)
+            height = (A / 2) * (1 - cos(2 * pi * (V * t - l) / L));
         else
-            outHeight = 0;
+            height = 0;
         end
         
-        heightProfile = [heightProfile, outHeight];
+        heightProfile(idx) = height;
+    
     end
 end
