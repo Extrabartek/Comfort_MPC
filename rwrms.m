e5939ab95fd15da62bf2c761f1512321c5d284c3
Rezoan <rezoanalom@gmail.com>
2024-06-06 15:11:43 +0200

    mtvv and vdv

--- rwrms.m
+++ rwrms.m
@@ -1,3 +1,9 @@
+% Running weighted root mean square
+% Inputs:
+%   a_z - array with vertical acceleration values
+%   ts - array with time of each acceleration value
+% Output:
+%   RWRMS - array of running weighted root mean square value of vertical acceleration time series
 function [RWRMS] = rwrms(a_z, ts)
     RWRMS = zeros(length(a_z));
     for i = 1:length(ts)
